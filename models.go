0e4f8b7170e480a8c5c7d025d3bd875738398647
fsbarbosa <6672383+fsbarbosa@users.noreply.github.com>
2024-06-16 12:47:16 +0200

    Add file models.go

---
+++ models.go
@@ -0,0 +1,12 @@
+package models
+
+import (
+	"time"
+)
+
+type Task struct {
+	ID          int       `json:"id"`          
+	Title       string    `json:"title"`       
+	Description string    `json:"description"` 
+	DueDate     time.Time `json:"dueDate"`     
+}
