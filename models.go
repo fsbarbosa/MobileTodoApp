38c908b87a087dcf6a4f3b2a6a7d3d3e4d466b23
fsbarbosa <6672383+fsbarbosa@users.noreply.github.com>
2024-06-19 01:04:39 +0200

    new update

--- models.go
+++ models.go
@@ -5,8 +5,13 @@ import (
 )
 
 type Task struct {
-	ID          int       `json:"id"`          
-	Title       string    `json:"title"`       
-	Description string    `json:"description"` 
-	DueDate     time.Time `json:"dueDate"`     
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	DueDate     time.Time `json:"dueDate"`
+}
+
+// IsOverdue returns true if the task due date is before the current time.
+func (t *Task) IsOverdue() bool {
+	return time.Now().After(t.DueDate)
 }
