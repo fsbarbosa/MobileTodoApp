af29911e8f46bc3ae1706d31f6ff46872e1e5a0c
fsbarbosa <6672383+fsbarbosa@users.noreply.github.com>
2024-06-20 09:22:50 +0200

    implemented feature

--- models.go
+++ models.go
@@ -1,17 +1,28 @@
 package models
 
 import (
-	"time"
+    "time"
 )
 
 type Task struct {
-	ID          int       `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	DueDate     time.Time `json:"dueDate"`
+    ID          int       `json:"id"`
+    Title       string    `json:"title"`
+    Description string    `json:"description"`
+    DueDate     time.Time `json:"dueDate"`
+    Complete    bool      `json:"complete"`
 }
 
 // IsOverdue returns true if the task due date is before the current time.
 func (t *Task) IsOverdue() bool {
-	return time.Now().After(t.DueDate)
+    return t.DueDate.Before(time.Now())
+}
+
+// UpdateDueDate updates the due date of the task.
+func (t *Task) UpdateDueDate(newDueDate time.Time) {
+    t.DueDate = newDueDate
+}
+
+// MarkComplete marks the task as completed.
+func (t *Task) MarkComplete() {
+    t.Complete = true
 }
